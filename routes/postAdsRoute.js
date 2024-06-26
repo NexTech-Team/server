1f8b18a4bd32dfd94177de7e65ad157ce365cee7
Rukshan Fernando <fernandownr.21@uom.lk>
2024-06-26 10:15:49 +0530

    comment postAds and postAdsRoute

--- routes/postAdsRoute.js
+++ routes/postAdsRoute.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const router = express.Router();
-const { postAd } = require("../controller/postAdsController");
-const verifyJWT = require("../middleware/verifyJWT");
+// const express = require("express");
+// const router = express.Router();
+// const { postAd } = require("../controller/postAdsController");
+// const verifyJWT = require("../middleware/verifyJWT");
 
-router.post("/ads", verifyJWT, postAd);
+// router.post("/ads", verifyJWT, postAd);
 
-module.exports = router;
+// module.exports = router;A
