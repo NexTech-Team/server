a887779951734dff458f22f5c4b546abc8446af5
Rukshan Fernando <fernandownr.21@uom.lk>
2024-06-28 00:41:59 +0530

    correct backend code as to allove fordifferent running type

--- index.js
+++ index.js
@@ -2,22 +2,30 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-require("dotenv").config(); // Ensure you have dotenv installed for local development
+const dotenv = require("dotenv");
+dotenv.config();
+
+// Set NODE_ENV to 'development' if not already set
+const nodeEnv = process.env.NODE_ENV || "production";
+require("dotenv").config({ path: `.env.${nodeEnv}` });
+
+console.log("Environment:", process.env.NODE_ENV);
 require("./utils/redisClient");
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 const advertismentRouter = require("./routes/advertisments");
 const authRouter = require("./routes/authRoute");
 const userRouter = require("./routes/userRoute");
-const postAdsRoute = require("./routes/postAdsRoute");
+// const postAdsRoute = require("./routes/postAdsRoute");
 const { sequelize } = require("./models");
 
-// Update CORS settings to use environment variable
-const allowedOrigins = [
-  "http://localhost:3000", // For local development
-  "https://www.carseek.live", // For production
-];
+// Parse ALLOWED_ORIGINS environment variable
+const allowedOrigins = process.env.ALLOWED_ORIGINS
+  ? process.env.ALLOWED_ORIGINS.split(",")
+  : [];
+console.log("Allowed origins:", allowedOrigins);
 
 app.use(
   cors({
@@ -38,7 +46,6 @@ app.use(cookieParser());
 app.use("/uploads", express.static("uploads"));
 
 // Routes
-app.use("/api", postAdsRoute);
 app.use("/api/advertisments/", advertismentRouter);
 app.use("/api/auth/", authRouter);
 app.use("/api/user/", userRouter);
@@ -48,7 +55,6 @@ app.use("/api/user/", userRouter);
     await sequelize.sync();
     console.log("Database synced successfully");
     app.listen(port, () => {
-      // Use the 'port' variable for the server to listen on
       console.log(`Server is running on port ${port}`);
     });
   } catch (err) {
