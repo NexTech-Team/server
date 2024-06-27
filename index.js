9f9dbb126bc5d9d9eda022e5d922aeacd3d911d9
Rukshan Fernando <fernandownr.21@uom.lk>
2024-06-28 05:18:08 +0530

    change index

--- index.js
+++ index.js
@@ -5,13 +5,6 @@ const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
 dotenv.config();
 
-// Set NODE_ENV to 'development' if not already set
-const nodeEnv = process.env.NODE_ENV || "production";
-require("dotenv").config({ path: `.env.${nodeEnv}` });
-
-console.log("Environment:", process.env.NODE_ENV);
-require("./utils/redisClient");
-
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -21,7 +14,12 @@ const userRouter = require("./routes/userRoute");
 
 const { sequelize } = require("./models");
 
-// Parse ALLOWED_ORIGINS environment variable
+// Health check endpoint
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
+// Configure CORS
 const allowedOrigins = process.env.ALLOWED_ORIGINS
   ? process.env.ALLOWED_ORIGINS.split(",")
   : [];
@@ -30,7 +28,7 @@ console.log("Allowed origins:", allowedOrigins);
 app.use(
   cors({
     origin: (origin, callback) => {
-      if (allowedOrigins.includes(origin) || !origin) {
+      if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
         callback(new Error("Not allowed by CORS"));
@@ -44,11 +42,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use("/uploads", express.static("uploads"));
-
-// Health check endpoint
-app.get("/health", (req, res) => {
-  res.status(200).send("OK");
-});
 
 // Routes
 app.use("/api/advertisments/", advertismentRouter);
