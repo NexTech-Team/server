be58f2606f1e591752f5793f75cba233cdd5969e
Rukshan Fernando <fernandownr.21@uom.lk>
2024-06-28 11:25:58 +0530

    change index.js

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
 dotenv.config();
 
+// Set NODE_ENV to 'development' if not already set
+const nodeEnv = process.env.NODE_ENV || "production";
+require("dotenv").config({ path: `.env.${nodeEnv}` });
+
+console.log("Environment:", process.env.NODE_ENV);
+require("./utils/redisClient");
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -14,21 +21,16 @@ const userRouter = require("./routes/userRoute");
 
 const { sequelize } = require("./models");
 
-// Health check endpoint
-app.get("/health", (req, res) => {
-  res.status(200).send("OK");
-});
-
-// Configure CORS
+// Parse ALLOWED_ORIGINS environment variable
 const allowedOrigins = process.env.ALLOWED_ORIGINS
   ? process.env.ALLOWED_ORIGINS.split(",")
-  : [];
+  : "https://www.carseek.live";
 console.log("Allowed origins:", allowedOrigins);
 
 app.use(
   cors({
     origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
+      if (allowedOrigins.includes(origin) || !origin) {
         callback(null, true);
       } else {
         callback(new Error("Not allowed by CORS"));
@@ -42,6 +44,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use("/uploads", express.static("uploads"));
+
+// Health check endpoint
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
 
 // Routes
 app.use("/api/advertisments/", advertismentRouter);
